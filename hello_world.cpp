d309f5c6cc919a821bb26a928d39977ca5a94682
Ignat Vaschilo <ignatvaschilo@yandex.ru>
2025-03-18 03:39:29 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-	std::string name;
+	std::string name; // тут имя хранится
 	std::cout << "Enter your name: ";
 	std::cin >> name; // ввод имени
 	std::cout << "Hello world from " << name << std::endl;
