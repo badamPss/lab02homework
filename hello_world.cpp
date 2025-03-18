11d4b2730577515fe2c69971a8e50b2d7b55874b
badamPss <ignatvaschilo@yandex.ru>
2025-03-18 03:18:18 +0300

    Add comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@
 int main() {
 	std::string name;
 	std::cout << "Enter your name: ";
-	std::cin >> name;
+	std::cin >> name; // ввод имени
 	std::cout << "Hello world from " << name << std::endl;
 	return 0;
 }
